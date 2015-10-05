6dc69bfa16fec8936b7d5c36960de4cfa3bd3edc
seoj <jmseo2@gmail.com>
2015-10-05 13:21:05 -0400

    Added basic project skeleton.

---
+++ travis-test-war/src/main/java/my/seoj/travis/MyWebApplicationInitializer.java
@@ -0,0 +1,15 @@
+package my.seoj.travis;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+import org.springframework.web.WebApplicationInitializer;
+
+public class MyWebApplicationInitializer implements WebApplicationInitializer
+{
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException
+    {
+        
+    }
+}
